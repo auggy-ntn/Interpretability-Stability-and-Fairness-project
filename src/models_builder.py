72bbbacd9c3f824e119cce7a17f558817fd5b43b
Augustin <augustin.naton@gmail.com>
2025-09-23 15:12:01 +0200

    Added xgboost model

--- src/models_builder.py
+++ src/models_builder.py
@@ -1,8 +1,10 @@
-from sklearn.linear_model import LinearRegression
 import matplotlib.pyplot as plt
 import numpy as np
+import xgboost as xgb
+from sklearn.linear_model import LinearRegression
 
 
+# A simple linear surrogate model for interpretability
 class LinearSurrogate:
     def __init__(self):
         self.model = LinearRegression()
@@ -12,19 +14,33 @@ class LinearSurrogate:
 
     def predict(self, X):
         return self.model.predict(X)
-    
+
     def get_coefficients(self):
         return self.model.coef_
-    
-    def plot_interpretation(self, features, max_features=10):
 
+    def plot_interpretation(self, features, max_features=10):
         coef = self.get_coefficients()
         indices = np.argsort(coef)
         indices = indices[-max_features:]
 
         plt.figure(figsize=(10, 6))
-        plt.barh(range(max_features), coef[indices], align='center')
+        plt.barh(range(max_features), coef[indices], align="center")
         plt.yticks(range(max_features), [features[i] for i in indices])
-        plt.xlabel('Coefficient Value')
-        plt.title('Feature Importance from Linear Surrogate Model')
+        plt.xlabel("Coefficient Value")
+        plt.title("Feature Importance from Linear Surrogate Model")
         plt.show()
+
+
+# Black Box XGBoost Model Wrapper
+class BlackBoxModel(xgb.XGBClassifier):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+    def fit(self, X, y):
+        super().fit(X, y)
+
+    def predict(self, X):
+        return super().predict(X)
+
+    def predict_proba(self, X):
+        return super().predict_proba(X)
