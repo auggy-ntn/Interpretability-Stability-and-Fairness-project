7fa9ed7ac383b1da3092b4ac25d3fabaf94b705d
Augustin <augustin.naton@gmail.com>
2025-09-24 20:59:26 +0200

    Restructured project for packaging

--- src/fair_credit_scoring/utils.py
+++ src/fair_credit_scoring/utils.py
@@ -2,12 +2,10 @@ import pickle
 from typing import List, Tuple, Union
 
 
-
-
 def unpack_model(model_name: str) -> object:
     """
     Unpack a serialized model from a pickle file.
-    Need to call an abslute path to the model.
+    Need to call an absolute path to the model.
     """
     model_path = f"models/{model_name}.pkl"
     with open(model_path, "rb") as f:
