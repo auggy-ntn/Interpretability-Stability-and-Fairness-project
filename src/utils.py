40da8803899e7b7e43c27ee8b1d6c19872003cda
Gustave Triomphe <gustave.triomphe@gmail.com>
2025-09-24 16:12:14 +0200

    changes

--- src/utils.py
+++ src/utils.py
@@ -3,13 +3,11 @@ from typing import List, Tuple, Union
 
 
 
-
-def unpack_model(model_name: str) -> object:
+def unpack_model(model_path: str) -> object:
     """
     Unpack a serialized model from a pickle file.
     Need to call an abslute path to the model.
     """
-    model_path = f"models/{model_name}.pkl"
     with open(model_path, "rb") as f:
         model = pickle.load(f)
     return model
