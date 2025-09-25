ee4a813302cd447f757405986833129d2c589f37
guillaumectr <guillaume.coutiere.2@gmail.com>
2025-09-25 15:13:06 +0200

    Fairness

---
+++ src/fairness.py
@@ -0,0 +1,52 @@
+from scipy.stats import chi2_contingency
+import pandas as pd
+import matplotlib.pyplot as plt
+
+def statistical_parity(df, feature, target):
+    """Calculate the Statistical Parity (SP) for a given feature."""
+    # Group by the feature and calculate the mean of the target
+    group_means = df.groupby(feature)[target].mean()
+    
+    # Calculate the absolute difference in means between groups
+    sp_value = abs(group_means.max() - group_means.min())
+    
+    return sp_value
+
+def fairness_test_statistic(df, model, protected_attribute):
+    """Calculate the fairness test statistic (SP or CSP) for a given feature."""
+    preds = model.predict(df)
+    contingency_table = pd.crosstab(df[protected_attribute], preds)
+    chi2, p, dof, expected = chi2_contingency(contingency_table)
+    return p
+
+def get_points_for_partial_dependance(df, feature, num_points=10):
+    """Get points for partial dependence plot."""
+    feature_values = df[feature]
+    if feature_values.nunique() <= num_points:
+        return feature_values.unique(), 'discrete'
+    min_val = feature_values.min()
+    max_val = feature_values.max()
+    return pd.Series([min_val + i * (max_val - min_val) / (num_points - 1) for i in range(num_points)]), 'continuous'
+
+def fairness_partial_dependance_plot(df, model, feature, protected_attribute, n_points=10):
+    """Generate a partial dependence plot for a given feature."""
+
+    df_temp = df.copy()
+    points, mode = get_points_for_partial_dependance(df, feature, n_points)
+    p_values = []
+    for point in points:
+        df_temp[feature] = point
+        p_value = fairness_test_statistic(df_temp, model, protected_attribute)
+        p_values.append(p_value)
+    
+    fig, ax = plt.subplots(figsize=(8, 6))
+    plt.title(f'Partial Dependence Plot for {feature}')
+    if mode == 'continuous':
+        plt.plot(points, p_values)
+    else:
+        plt.bar(points, p_values, align='center')
+
+    plt.xlabel(feature)
+    plt.ylabel('Fairness Test Statistic (p-value)')
+    plt.grid()
+    plt.show()
